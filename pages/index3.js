3edbdbab56d7646d1dff8aa1c3810d9ca9caa783
Jay Price <jay.price@morelandconnect.com>
2021-04-08 01:38:25 -0400

    Fixing errors

--- pages/index3.js
+++ pages/index3.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Navbar from '../components/_App/Navbar';
 import MainBanner from '../components/HomeThree/MainBanner';
 import About from '../components/HomeThree/About';
-import VideoCreated from '../components/HomeThree/VideoCreated';
 import Testimonials from '../components/HomeThree/Testimonials';
 import UpcomingMovies from '../components/HomeThree/UpcomingMovies';
 import WhatWeDo from '../components/HomeThree/WhatWeDo';
@@ -10,27 +9,22 @@ import TeamMember from '../components/HomeThree/TeamMember';
 import ProductionProcess from '../components/HomeThree/ProductionProcess';
 import LatestNews from '../components/HomeThree/LatestNews';
 import Partners from '../components/Common/Partners';
-import AwardWinningMovies from '../components/HomeThree/AwardWinningMovies';
 import Footer from '../components/_App/Footer';
 
-const Index3 = () => {
-    return (
-        <React.Fragment>
-            <Navbar />
-            <MainBanner />
-            <About />
-            <VideoCreated />
-            <Testimonials />
-            <UpcomingMovies />
-            <WhatWeDo />
-            <TeamMember />
-            <ProductionProcess />
-            <AwardWinningMovies />
-            <LatestNews />
-            <Partners />
-            <Footer />
-        </React.Fragment>
-    )
-}
+const Index3 = () => (
+  <>
+    <Navbar />
+    <MainBanner />
+    <About />
+    <Testimonials />
+    <UpcomingMovies />
+    <WhatWeDo />
+    <TeamMember />
+    <ProductionProcess />
+    <LatestNews />
+    <Partners />
+    <Footer />
+  </>
+);
 
 export default Index3;
